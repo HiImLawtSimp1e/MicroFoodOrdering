37e110de378c163a959c21cf5779166560cc2426
hungnqph19112 <nqh1703k2@gmail.com>
2025-01-20 16:41:24 +0700

    pritier code

--- MangoFood.UI/Services/Service/ProductService.cs
+++ MangoFood.UI/Services/Service/ProductService.cs
@@ -5,68 +5,68 @@ using MangoFood.UI.Utilities;
 
 namespace MangoFood.UI.Services.Service
 {
-	public class ProductService : IProductService
-	{
-		private readonly IBaseService _baseService;
+    public class ProductService : IProductService
+    {
+        private readonly IBaseService _baseService;
 
-		public ProductService(IBaseService baseService)
+        public ProductService(IBaseService baseService)
         {
-			_baseService = baseService;
-		}
+            _baseService = baseService;
+        }
         public async Task<ResponseDto?> CreateProductsAsync(CreateProductDto productDto)
-		{
-			return await _baseService.SendAsync(new RequestDto()
-			{
-				ApiType = SD.ApiType.POST,
-				Data = productDto,
-				Url = SD.ProductAPIBase + "/Product"
-			});
-		}
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = productDto,
+                Url = SD.ProductAPIBase + "/Product"
+            });
+        }
 
-		public async Task<ResponseDto?> DeleteProductsAsync(Guid id)
-		{
-			return await _baseService.SendAsync(new RequestDto()
-			{
-				ApiType = SD.ApiType.DELETE,
-				Url = SD.ProductAPIBase + "/Product/" + id
-			});
-		}
+        public async Task<ResponseDto?> DeleteProductsAsync(Guid id)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.DELETE,
+                Url = SD.ProductAPIBase + "/Product/" + id
+            });
+        }
 
-		public async Task<ResponseDto?> GetProductsAsync()
-		{
-			return await _baseService.SendAsync(new RequestDto()
-			{
-				ApiType = SD.ApiType.GET,
-				Url = SD.ProductAPIBase + "/Product"
-			});
-		}
+        public async Task<ResponseDto?> GetProductsAsync()
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.ProductAPIBase + "/Product"
+            });
+        }
 
-		public async Task<ResponseDto?> GetProductAsync(string productCode)
-		{
-			return await _baseService.SendAsync(new RequestDto()
-			{
-				ApiType = SD.ApiType.GET,
-				Url = SD.ProductAPIBase + "/Product/GetByCode/" + productCode
-			});
-		}
+        public async Task<ResponseDto?> GetProductAsync(string productCode)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.ProductAPIBase + "/Product/GetByCode/" + productCode
+            });
+        }
 
-		public async Task<ResponseDto?> GetProductByIdAsync(Guid id)
-		{
-			return await _baseService.SendAsync(new RequestDto()
-			{
-				ApiType = SD.ApiType.GET,
-				Url = SD.ProductAPIBase + "/Product/" + id
-			});
-		}
+        public async Task<ResponseDto?> GetProductByIdAsync(Guid id)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.ProductAPIBase + "/Product/" + id
+            });
+        }
 
-		public async Task<ResponseDto?> UpdateProductsAsync(Guid id, UpdateProductDto productDto)
-		{
-			return await _baseService.SendAsync(new RequestDto()
-			{
-				ApiType = SD.ApiType.PUT,
-				Data = productDto,
-				Url = SD.ProductAPIBase + "/Product/" + id
-			});
-		}
-	}
+        public async Task<ResponseDto?> UpdateProductsAsync(Guid id, UpdateProductDto productDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.PUT,
+                Data = productDto,
+                Url = SD.ProductAPIBase + "/Product/" + id
+            });
+        }
+    }
 }
