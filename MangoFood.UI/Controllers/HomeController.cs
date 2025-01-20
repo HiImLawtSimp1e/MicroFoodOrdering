b77d4cf2055bded4cf94a381f4c9f46d8dc7067d
hungnqph19112 <nqh1703k2@gmail.com>
2025-01-20 16:39:36 +0700

    FE Customer UI

--- MangoFood.UI/Controllers/HomeController.cs
+++ MangoFood.UI/Controllers/HomeController.cs
@@ -1,21 +1,53 @@
 using MangoFood.UI.Models;
+using MangoFood.UI.Models.DTOs.Common;
+using MangoFood.UI.Models.DTOs.ProductDTO;
+using MangoFood.UI.Services.IService;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace MangoFood.UI.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IProductService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IProductService productService)
         {
-            _logger = logger;
+            _productService = productService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var products = new List<ProductResponseDto>();
+
+            ResponseDto? response = await _productService.GetProductsAsync();
+
+            if (response != null && response.Success)
+            {
+                products = JsonConvert.DeserializeObject<List<ProductResponseDto>>(JsonConvert.SerializeObject(response.Data));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+            return View(products);
+        }
+        public async Task<IActionResult> ProductDetails(Guid productId)
+        {
+            var product = new ProductResponseDto();
+
+            ResponseDto? response = await _productService.GetProductByIdAsync(productId);
+            if (response != null && response.Success)
+            {
+                product = JsonConvert.DeserializeObject<ProductResponseDto>(Convert.ToString(response.Data));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+            return View(product);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- MangoFood.UI/Models/DTOs/ProductDTO/ProductResponseDto.cs
+++ MangoFood.UI/Models/DTOs/ProductDTO/ProductResponseDto.cs
@@ -1,4 +1,6 @@
-﻿namespace MangoFood.UI.Models.DTOs.ProductDTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MangoFood.UI.Models.DTOs.ProductDTO
 {
     public class ProductResponseDto
     {
@@ -8,5 +10,7 @@
         public string Description { get; set; }
         public string CategoryName { get; set; }
         public string ImageUrl { get; set; }
+        [Range(1, 100)]
+        public int Count { get; set; } = 1;
     }
 }
